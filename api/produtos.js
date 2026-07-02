a216387c7e2e292a15a276285b5272a1489e2b91
stwarth13-design <stwarth13@gmail.com>
2026-07-02 01:36:07 -0300

    Update produtos.js

--- api/produtos.js
+++ api/produtos.js
@@ -1,6 +1,14 @@
 const FACILZAP_BASE = 'https://api.facilzap.app.br';
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   const token = process.env.FACILZAP_TOKEN;
 
   if (!token) {
